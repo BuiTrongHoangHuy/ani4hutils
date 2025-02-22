568991511e1ef8ae8eddc50fd5ca70867dc9ab47
caovanhoang63 <22520457@gm.uit.edu.vn>
2025-02-22 20:31:07 +0700

    test copy object

--- ffmpeg/main.go
+++ ffmpeg/main.go
@@ -32,9 +32,11 @@ func handleRequest(ctx context.Context, event events.S3Event) error {
 	for _, record := range event.Records {
 		fmt.Println(record.S3.Bucket.Name, record.S3.Object.Key)
 
+		sourceBucket := record.S3.Bucket.Name
+		objectKey := record.S3.Object.Key
 		param := &s3.CopyObjectInput{
 			Bucket:     aws.String("ani4h-film-storage"),
-			CopySource: &record.S3.Bucket.Name,
+			CopySource: aws.String(fmt.Sprintf("%v/%v", sourceBucket, objectKey)),
 			Key:        &record.S3.Object.Key,
 		}
 		_, err := s3Client.CopyObject(ctx, param)
