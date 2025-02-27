ff967525f5f2c7115c40c618446a3518c8a1d444
caovanhoang63 <22520457@gm.uit.edu.vn>
2025-02-27 16:22:46 +0700

    feat: add master lambda to analyst the file metadata

---
+++ ffmpeg/master/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"log"
+)
+
+var (
+	s3Client *s3.Client
+)
+
+func init() {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	s3Client = s3.NewFromConfig(cfg)
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
+
+func handleRequest(ctx context.Context, event events.S3Event) (string, error) {
+	for _, record := range event.Records {
+		sourceBucket := record.S3.Bucket.Name
+		objectKey := record.S3.Object.Key
+
+		output, err := s3Client.HeadObject(ctx, &s3.HeadObjectInput{
+			Bucket: aws.String(sourceBucket),
+			Key:    aws.String(objectKey),
+		})
+		if err != nil {
+			return "", err
+		}
+
+		if resolution, ok := output.Metadata["resolution"]; ok {
+			return resolution, nil
+		}
+		return "", nil
+	}
+	return "empty", nil
+}
