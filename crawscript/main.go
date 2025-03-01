5b5b7a72539889c3a4fd995cfcb555f788626a89
caovanhoang63 <22520457@gm.uit.edu.vn>
2025-03-01 16:30:51 +0700

    chore: no need to clean film's title

--- crawscript/main.go
+++ crawscript/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -12,8 +11,6 @@ import (
 func main() {
 	// variable c is a collector
 	title := ""
-
-
 	c := colly.NewCollector()
 	d := colly.NewCollector()
 
@@ -54,100 +51,95 @@ func main() {
 	c.OnHTML(`div.leftside > div.spaceit_pad`, func(e *colly.HTMLElement) {
 		spanText := e.ChildText("span:first-child")
 		switch spanText {
-			case "Synonyms:":
-				remainText:= strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
-				fmt.Println("Synonyms: " + remainText)
-			case "Japanese:":
-				remainText:= strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
-				fmt.Println("Japanese: " + remainText)
-			case "Type:":
-				fmt.Println("type: " + e.ChildText("a"))
-			case "Episodes:":
-				remainText:= strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
-				fmt.Println("Episodes: " + remainText)
-			case "Status:":
-				remainText:= strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
-				fmt.Println("Status: " + remainText)
-			case "Aired:":
-				remainText:= strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
-				fmt.Println("Aired: " + remainText)
-			case "Premiered:":
-				fmt.Println("premiered: " + e.ChildText("a"))
-			case "Broadcast:":
-				remainText:= strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
-				fmt.Println("Broadcast: " + remainText)
-			case "Producers:":
-				remainText:= strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
-				parts := strings.Split(remainText, ",")
-				for i := range parts {
-					parts[i] = strings.TrimSpace(parts[i])
-				}
-				remainText = strings.Join(parts, ", ")
-				fmt.Println("Producers: " + remainText)
-			case "Licensors:":
-				remainText:= strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
-				parts := strings.Split(remainText, ",")
-				for i := range parts {
-					parts[i] = strings.TrimSpace(parts[i])
-				}
-				remainText = strings.Join(parts, ", ")
-				fmt.Println("Licensors: " + remainText)
-			case "Studios:":
-				remainText:= strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
-				parts := strings.Split(remainText, ",")
-				for i := range parts {
-					parts[i] = strings.TrimSpace(parts[i])
-				}
-				remainText = strings.Join(parts, ", ")
-				fmt.Println("Studios: " + remainText)
-			case "Source:":
-				fmt.Println("source: " + e.ChildText("a"))
-			case "Genres:":
-				remainText:= strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
-				parts := strings.Split(remainText, ",")
-				for i := range parts {
-					parts[i] = strings.TrimSpace(parts[i])
-				}
-				remainText = strings.Join(parts, ", ")
-				fmt.Println("Genres: " + remainText)
-			case "Duration:":
-				remainText:= strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
-				fmt.Println("Duration: " + remainText)
-			case "Rating:":
-				remainText:= strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
-				fmt.Println("Rating: " + remainText)
+		case "Synonyms:":
+			remainText := strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
+			fmt.Println("Synonyms: " + remainText)
+		case "Japanese:":
+			remainText := strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
+			fmt.Println("Japanese: " + remainText)
+		case "Type:":
+			fmt.Println("type: " + e.ChildText("a"))
+		case "Episodes:":
+			remainText := strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
+			fmt.Println("Episodes: " + remainText)
+		case "Status:":
+			remainText := strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
+			fmt.Println("Status: " + remainText)
+		case "Aired:":
+			remainText := strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
+			fmt.Println("Aired: " + remainText)
+		case "Premiered:":
+			fmt.Println("premiered: " + e.ChildText("a"))
+		case "Broadcast:":
+			remainText := strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
+			fmt.Println("Broadcast: " + remainText)
+		case "Producers:":
+			remainText := strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
+			parts := strings.Split(remainText, ",")
+			for i := range parts {
+				parts[i] = strings.TrimSpace(parts[i])
+			}
+			remainText = strings.Join(parts, ", ")
+			fmt.Println("Producers: " + remainText)
+		case "Licensors:":
+			remainText := strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
+			parts := strings.Split(remainText, ",")
+			for i := range parts {
+				parts[i] = strings.TrimSpace(parts[i])
+			}
+			remainText = strings.Join(parts, ", ")
+			fmt.Println("Licensors: " + remainText)
+		case "Studios:":
+			remainText := strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
+			parts := strings.Split(remainText, ",")
+			for i := range parts {
+				parts[i] = strings.TrimSpace(parts[i])
+			}
+			remainText = strings.Join(parts, ", ")
+			fmt.Println("Studios: " + remainText)
+		case "Source:":
+			fmt.Println("source: " + e.ChildText("a"))
+		case "Genres:":
+			remainText := strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
+			parts := strings.Split(remainText, ",")
+			for i := range parts {
+				parts[i] = strings.TrimSpace(parts[i])
+			}
+			remainText = strings.Join(parts, ", ")
+			fmt.Println("Genres: " + remainText)
+		case "Duration:":
+			remainText := strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
+			fmt.Println("Duration: " + remainText)
+		case "Rating:":
+			remainText := strings.TrimSpace(strings.Replace(e.Text, spanText, "", 1))
+			fmt.Println("Rating: " + remainText)
 		}
 	})
-	
 
 	c.Visit("https://myanimelist.net/anime/58567/")
 
-
 	// get characters & staff
-	d.OnHTML(`div.anime-character-container.js-anime-character-container > table`, 
-	func(e *colly.HTMLElement) {
-		image := e.DOM.Find("div.picSurround > a > img").AttrOr("data-src", "")
-		name := e.DOM.Find("h3.h3_character_name").Text()
-		role := e.DOM.Find("tbody > tr > td:nth-child(2) > div:nth-child(4)").Text()
-		role = strings.TrimSpace(role)
-		fmt.Println("name: " + name + " role: " + role + " image: " + image)
-		// get voice actor
-		e.DOM.Find("tr.js-anime-character-va-lang").Each(func(i int, s *goquery.Selection) {
-			image_voiceActor := s.Find("div.picSurround > a > img").AttrOr("data-src", "")
-			name_voiceActor := s.Find("div.spaceit_pad > a").Text()
-			language := s.Find("div.spaceit_pad.js-anime-character-language").Text()
-			language = strings.TrimSpace(language)
-			fmt.Println("voice_actor: " + name_voiceActor + "language: " + language + " image: " + image_voiceActor)
+	d.OnHTML(`div.anime-character-container.js-anime-character-container > table`,
+		func(e *colly.HTMLElement) {
+			image := e.DOM.Find("div.picSurround > a > img").AttrOr("data-src", "")
+			name := e.DOM.Find("h3.h3_character_name").Text()
+			role := e.DOM.Find("tbody > tr > td:nth-child(2) > div:nth-child(4)").Text()
+			role = strings.TrimSpace(role)
+			fmt.Println("name: " + name + " role: " + role + " image: " + image)
+			// get voice actor
+			e.DOM.Find("tr.js-anime-character-va-lang").Each(func(i int, s *goquery.Selection) {
+				image_voiceActor := s.Find("div.picSurround > a > img").AttrOr("data-src", "")
+				name_voiceActor := s.Find("div.spaceit_pad > a").Text()
+				language := s.Find("div.spaceit_pad.js-anime-character-language").Text()
+				language = strings.TrimSpace(language)
+				fmt.Println("voice_actor: " + name_voiceActor + "language: " + language + " image: " + image_voiceActor)
 
+			})
 		})
-	})
 	// convert title
-	re := regexp.MustCompile(`[^a-zA-Z0-9\s]`)
-	cleaned := re.ReplaceAllString(title, "")
-	// Thay khoảng trắng giữa các từ thành dấu "_"
-	result := strings.ReplaceAll(cleaned, " ", "_")
-	url := fmt.Sprintf("https://myanimelist.net/anime/58567/%s/characters", result)
-	fmt.Println(url)
-	d.Visit(url)
+	//re := regexp.MustCompile(`[^a-zA-Z0-9\s]`)
+	//cleaned := re.ReplaceAllString(title, "")
+	//// Thay khoảng trắng giữa các từ thành dấu "_"
+	//result := strings.ReplaceAll(cleaned, " ", "_")
+	d.Visit("https://myanimelist.net/anime/58567/a/characters")
 }
-
