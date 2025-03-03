2a01e95bfa1e207dcc15f7937e65b9953c95074a
Bui Thai Hoang <buithaihoang04gl@gmail.com>
2025-03-04 01:33:35 +0700

    Implement Jwt and spring security

---
+++ api/src/main/java/site/ani4h/api/middleware/jwt_spring_security/JwtUtils.java
@@ -0,0 +1,68 @@
+package site.ani4h.api.middleware.jwt_spring_security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+import static io.grpc.Context.key;
+
+@Component
+public class JwtUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    private String jwtSecret = "P7nK4vQ9mRwT2uJ5aY8eD3bHsZ6iL1xO";
+    private int accessTokenExpirationMs = 1000 * 60 * 30;
+    private int refreshTokenExpirationMs = 1000 * 60 * 60 * 24 * 7;
+
+    public String generateJwtToken(Authentication authentication, long jwtExpirationInMs) {
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getEmail()))
+                .setIssuedAt(java.util.Date.from(java.time.Instant.now()))
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationInMs))
+                .signWith(key(),SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // generateAccessToken
+    public String generateAccessToken(Authentication authentication) {
+        return generateJwtToken(authentication, accessTokenExpirationMs);
+    }
+    // generateRefreshToken
+    public String generateRefreshToken(Authentication authentication) {
+        return generateJwtToken(authentication, refreshTokenExpirationMs);
+    }
+    // get email from token
+    public String getEmailFromJwtToken(String token){
+        return Jwts.parserBuilder().setSigningKey(key()).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
+    }
+}
---
+++ api/src/main/java/site/ani4h/api/middleware/jwt_spring_security/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package site.ani4h.api.middleware.jwt_spring_security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.ani4h.api.auth.Auth;
+import site.ani4h.api.auth.AuthRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final AuthRepository authRepository;
+    @Autowired
+    public UserDetailsServiceImpl(AuthRepository authRepository) {
+        super();
+        this.authRepository = authRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        Auth auth = this.authRepository.findByEmail(email);
+        if (auth == null) {
+            throw new UsernameNotFoundException("User not found with username: " + email);
+        } else {
+            return UserDetailsImpl.build(auth.getId(),auth.getUserId(),auth.getEmail(),auth.getPassword(),auth.getSalt());
+        }
+    }
+}
