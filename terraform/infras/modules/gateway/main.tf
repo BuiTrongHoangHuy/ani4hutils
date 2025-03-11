d9e74b5fe6d7519b4e5d5af8665b7ae513266f24
caovanhoang63 <22520457@gm.uit.edu.vn>
2025-03-11 17:00:01 +0700

    feat: change to use http gateway

--- terraform/infras/modules/gateway/main.tf
+++ terraform/infras/modules/gateway/main.tf
@@ -5,14 +5,9 @@ terraform {
     }
   }
 }
-resource "aws_api_gateway_rest_api" "rest_gateway" {
+resource "aws_apigatewayv2_api" "rest_gateway" {
   name        = "${var.project}-gateway"
   description = "This is my API for demonstration purposes"
-}
-
-resource "aws_api_gateway_resource" "auth" {
-  path_part   = "auth"
-  parent_id   = aws_api_gateway_rest_api.rest_gateway.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.rest_gateway.id
+  protocol_type = "HTTP"
 }
 
