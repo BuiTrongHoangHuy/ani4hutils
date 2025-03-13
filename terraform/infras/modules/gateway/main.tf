bceeaab492ec40e22ee83b0e65285aadb0fa4141
caovanhoang63 <22520457@gm.uit.edu.vn>
2025-03-13 15:18:37 +0700

    chore: stage v1

--- terraform/infras/modules/gateway/main.tf
+++ terraform/infras/modules/gateway/main.tf
@@ -5,9 +5,25 @@ terraform {
     }
   }
 }
+
+
+#
+# resource "aws_apigatewayv2_domain_name" "domain" {
+#   domain_name = "api.ani4h.site"
+#   depends_on = [aws_acm_certificate.cert]
+#   domain_name_configuration {
+#     certificate_arn = aws_acm_certificate.cert.arn
+#     endpoint_type   = "REGIONAL"
+#     security_policy = "TLS_1_2"
+#   }
+# }
 resource "aws_apigatewayv2_api" "rest_gateway" {
   name        = "${var.project}-gateway"
   description = "This is my API for demonstration purposes"
   protocol_type = "HTTP"
 }
 
+resource "aws_apigatewayv2_stage" "v1" {
+  api_id = aws_apigatewayv2_api.rest_gateway.id
+  name   = "v1"
+}
