304853a0b3e74aa927f876727000c958052804b7
caovanhoang63 <22520457@gm.uit.edu.vn>
2025-03-24 17:55:00 +0700

    feat(client-ui): carousel pagination and navigation buttons

--- client-ui/src/app/carousel.tsx
+++ client-ui/src/app/carousel.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+import {Autoplay, Navigation, Pagination} from "swiper/modules";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -11,22 +11,31 @@ export default function Carousel({ className }: { className: string }) {
     return (
         <Swiper
             className={`${className}`}
-            modules={[Navigation, Pagination]}
+            modules={[Navigation, Pagination,Autoplay]}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
-            navigation
             pagination={{ clickable: true,
-                bulletClass: 'swiper-pagination-bullet mx-1 w-2 h-2 bg-gray-400 rounded-full transition-all duration-300',
-                // Class cho bullet active - dài hơn
-                bulletActiveClass: 'swiper-pagination-bullet-active w-6 h-2 bg-blue-600 rounded-full'}}
+                horizontalClass: "carousel-horizontal-pagination",
+                bulletElement: "div",
+                bulletClass: 'w-4 rounded-4xl mx-2 h-4 bg-gray-400 opacity-50 inline-block',
+                bulletActiveClass: 'w-8'}}
             slidesPerView={1}
+            navigation={{
+            }}
+            autoplay={
+            {
+                delay: 3000
+            }
+            }
             loop={true}
         >
             <SwiperSlide>
-                <div className="flex flex-col h-full w-full bg-[url('/love-is-war.jpg')] bg-cover bg-center box-border shadow-[inset_0_0_150px_100px_#000000]">
+                <div className="flex  flex-col h-full w-full bg-[url('/love-is-war.jpg')] bg-cover bg-center box-border shadow-[inset_0_0_150px_100px_#000000]">
                     <div className={"mt-auto p-20 "}>
-                        <Link href={"https://animevietsub.lol/phim/cuoc-chien-to-tinh-a3328/"}
-                              className={"text-2xl font-bold drop-shadow-2xl mb-20"}>Kaguya love is war</Link>
+                        <div  className={"text-2xl font-bold drop-shadow-2xl mb-10"}>
+                            <Link href={"https://animevietsub.lol/phim/cuoc-chien-to-tinh-a3328/"}
+                            >Kaguya love is war</Link>
+                        </div>
                         <div className={"flex gap-4"}>
                             <div className={"btn btn-primary rounded-xs"}>vip</div>
                             <div className={"btn opacity-60 rounded-xs text-white"}>Rom-com</div>
