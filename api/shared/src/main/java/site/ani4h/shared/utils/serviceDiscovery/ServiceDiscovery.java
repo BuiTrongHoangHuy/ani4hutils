a029084dd68f2f462f6851c95f37e897dd078d24
caovanhoang63 <22520457@gm.uit.edu.vn>
2025-03-29 10:53:50 +0700

    refactor:

--- api/shared/src/main/java/site/ani4h/shared/utils/serviceDiscovery/ServiceDiscovery.java
+++ api/shared/src/main/java/site/ani4h/shared/utils/serviceDiscovery/ServiceDiscovery.java
@@ -1,10 +1,11 @@
 package site.ani4h.shared.utils.serviceDiscovery;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component
+@Service
 public interface ServiceDiscovery {
     ServiceInstance getServiceInstance(String service);
     List<ServiceInstance> getAllServiceInstances(String service);
