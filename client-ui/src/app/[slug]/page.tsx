aa918d0b86b387cf43687a66d33656c03f48c4f2
caovanhoang63 <22520457@gm.uit.edu.vn>
2025-04-01 14:47:08 +0700

    fix(client-ui):

--- client-ui/src/app/[slug]/page.tsx
+++ client-ui/src/app/[slug]/page.tsx
@@ -3,6 +3,7 @@ import {film} from "@/types/film";
 import {FilmList} from "@/types/filmList";
 import ListFilm from "@/components/ListFilm";
 import Link from "next/link";
+import {url} from "@/types/cons";
 
 export default async function Page({
                                        params,
@@ -11,9 +12,9 @@ export default async function Page({
 }) {
     const { slug } = await params
     const data = await fetch(`${url}/v1/film/${slug.split('-')[slug.split('-').length-1]}`)
-    const film : film = (await data.json()).data
-    const filmData = await fetch(`${url}/v1/film`)
-    const films : FilmList[] = (await filmData.json()).data
+    const filmData : film = (await data.json()).data
+    const filmsData = await fetch(`${url}/v1/film`)
+    const films : FilmList[] = (await filmsData.json()).data
     return (
         <div className={"w-screen mt-[64px] px-20 py-10 space-y-20 "}>
 
@@ -23,31 +24,31 @@ export default async function Page({
                     <Image
                         className="rounded-md object-fill"
                         fill={true}
-                        src={film.images?.[0]?.url || "https://placehold.co/300x400/png?text=ani4h.site"}
-                        alt={film.title}
+                        src={filmData.images?.[0]?.url || "https://placehold.co/300x400/png?text=ani4h.site"}
+                        alt={filmData.title}
                     />
                 </div>
                 <div className={"space-y-4  w-2/3 font-bold"}>
-                    <h1 className={"text-3xl font-bold"}>{film?.title}</h1>
+                    <h1 className={"text-3xl font-bold"}>{filmData?.title}</h1>
                     <div className={"flex space-x-2"}>
                         <Image src={`/images/icons/start.svg`} alt={""} width="16" height="16"/>
-                        <p className={"text-primary"}>{film.avgStar || 0}</p>
+                        <p className={"text-primary"}>{filmData.avgStar || 0}</p>
                         <div className={"divider divider-horizontal"}></div>
-                        {film.season && <p>{film.season}</p>}
-                        <p>{film.year || new Date(film.startDate || Date.now()).getUTCFullYear()}</p>
+                        {filmData.season && <p>{filmData.season}</p>}
+                        <p>{filmData.year || new Date(filmData.startDate || Date.now()).getUTCFullYear()}</p>
                         <div className={"divider divider-horizontal"}></div>
-                        <p>Update to {film.numEpisodes} episodes</p>
+                        <p>Update to {filmData.numEpisodes} episodes</p>
                         <div className={"divider divider-horizontal"}></div>
-                        <p>Full {film.maxEpisodes} episodes</p>
+                        <p>Full {filmData.maxEpisodes} episodes</p>
                     </div>
                     <div className={"flex space-x-2"}>
                         <p>Category: </p>
-                        {film.genres.map((g, i) =>
+                        {filmData.genres.map((g, i) =>
                             <Link key={i} href={`/film?genre=${g.id}`}>{g.name}</Link>
                         )}
                     </div>
-                    <p className={"font-bold"}>{film?.synopsis}</p>
-                    <p>Views: {film.view || 0}</p>
+                    <p className={"font-bold"}>{filmData?.synopsis}</p>
+                    <p>Views: {filmData.view || 0}</p>
                     <div className={"flex w-full justify-end space-x-5"}>
                         <div className={"btn btn-primary text-2xl px-8 py-6"}>
                             <Image src={`/images/icons/img.png`} alt={""} width="16" height="16"/>
