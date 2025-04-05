e680f9c1dd8ee5c95675d98209b4482dd31f02d2
caovanhoang63 <22520457@gm.uit.edu.vn>
2025-04-05 23:26:20 +0700

    feat(client-ui): login, signup modal and call login api

---
+++ client-ui/src/app/api/login/route.ts
@@ -0,0 +1,26 @@
+import {NextRequest, NextResponse} from "next/server";
+import {url} from "@/types/cons";
+import {cookies} from "next/headers";
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const options: RequestInit & { duplex: 'half' } = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+        duplex: 'half',
+    };
+
+    const loginRes = await fetch(url + '/v1/auth/login', options);
+    const data = (await loginRes.json());
+    console.log(data);
+    (await cookies()).set("accessToken", data.data.accessToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60,
+        sameSite: "strict"
+    });
+
+    return NextResponse.json(data.data, { status: 201 });
+}
