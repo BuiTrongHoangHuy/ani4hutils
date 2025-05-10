a36bc5a230aba2bf8ebe9ae93511b93874279121
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2025-05-10 18:54:13 +0700

    feat(client-ui): map api getEpisode

--- client-ui/src/app/film/[slug]/[id]/PlayerWrapper.tsx
+++ client-ui/src/app/film/[slug]/[id]/PlayerWrapper.tsx
@@ -3,6 +3,6 @@ import dynamic from "next/dynamic";
 
 const Player = dynamic(() => import('@/app/film/[slug]/[id]/player'), { ssr: false });
 
-export default function PlayerWrapper() {
-    return <Player></Player>
+export default function PlayerWrapper({filmId, episodeNumber}: { filmId: string, episodeNumber:number}) {
+    return <Player filmId={filmId} episodeNumber={episodeNumber}></Player>
 }
--- client-ui/src/types/cons.ts
+++ client-ui/src/types/cons.ts
@@ -1,1 +1,2 @@
 export const url = "https://api.ani4h.site"
+export const url2 = "http://localhost:4002"
